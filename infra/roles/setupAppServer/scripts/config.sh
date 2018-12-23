856cd5dae19bb78345286a4569c68ec277a29589
vullinitsh@gmail.com <vullinitsh@gmail.com>
2018-12-23 18:07:48 +0530

    ignore pem file

--- infra/roles/setupAppServer/scripts/config.sh
+++ infra/roles/setupAppServer/scripts/config.sh
@@ -4,6 +4,5 @@ sudo apt  install docker.io
 sudo usermod -aG docker ubuntu
 sudo service docker start
 sudo apt install git
-git clone https://github.com/nitishvu/MeanStackDockerAwsApp.git
 cd MeanStackDockerAwsApp/app
 docker-compose up -d
